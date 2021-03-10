f767ad2d82910c3e9ed6c92796fb3fac8f787181
project-work-cap <project.work.cap@gmail.com>
2021-03-10 14:57:22 +0530

    Trying resolving main class

--- OnlineBookStore/src/main/java/com/capgemini/onlinebookstore/OnlineBookStoreApplication.java
+++ OnlineBookStore/src/main/java/com/capgemini/onlinebookstore/OnlineBookStoreApplication.java
@@ -2,14 +2,8 @@ package com.capgemini.onlinebookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
-@EnableTransactionManagement
 public class OnlineBookStoreApplication {
 
 	public static void main(String[] args) {
@@ -17,3 +11,4 @@ public class OnlineBookStoreApplication {
 	}
 
 }
+
