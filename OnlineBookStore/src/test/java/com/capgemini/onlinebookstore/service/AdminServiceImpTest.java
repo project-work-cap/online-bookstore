6a657454b486fe43cb3101fe5a863cbc14fffcc7
project-work-cap <project.work.cap@gmail.com>
2021-04-05 13:07:53 +0530

    Implemented Category test class

--- OnlineBookStore/src/test/java/com/capgemini/onlinebookstore/service/AdminServiceImpTest.java
+++ OnlineBookStore/src/test/java/com/capgemini/onlinebookstore/service/AdminServiceImpTest.java
@@ -1,14 +1,23 @@
 package com.capgemini.onlinebookstore.service;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 
 import com.capgemini.onlinebookstore.dao.AdminDao;
+import com.capgemini.onlinebookstore.domain.CategoryDto;
+import com.capgemini.onlinebookstore.entities.Category;
+import com.capgemini.onlinebookstore.exception.InvalidDataException;
 import com.capgemini.onlinebookstore.mapper.CategoryBookConverter;
 
 @ExtendWith(SpringExtension.class)
@@ -22,14 +31,27 @@ class AdminServiceImpTest {
 	
 	@Mock
 	CategoryBookConverter converter;
-	/*
+	
+	@InjectMocks
+	AdminServiceImp service=new AdminServiceImp();
+	
+	
 	@Test
-	void addCategorytest() {
-		fail("Not yet implemented");
-	}
-	*/
-	@Test 
-	public void tempTest() {
-		assertEquals(1,1);
+	public void addCategoryTest() throws InvalidDataException {
+		CategoryDto dto=new CategoryDto();
+		dto.setCategoryName("Thriller");
+		dto.setDescription("Regarding thriller");
+		dto.setRating("good");
+		
+		Errors errors = new BeanPropertyBindingResult(dto, "dto");
+		Category category=new Category();
+		
+		doNothing().when(validation).validate(dto, errors);
+		when(converter.dtoToModel(dto)).thenReturn(category);
+		when(dao.findByCategoryName(dto.getCategoryName())).thenReturn(null);
+		when(dao.save(category)).thenReturn(category);
+		
+		CategoryDto result=service.addCategory(dto, (BindingResult) errors);
+		assertEquals(dto, result);
 	}
 }
--- OnlineBookStore/src/test/java/com/capgemini/onlinebookstore/service/RegistrationServiceTest.java
+++ OnlineBookStore/src/test/java/com/capgemini/onlinebookstore/service/RegistrationServiceTest.java
@@ -8,20 +8,22 @@ import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
-
 import com.capgemini.onlinebookstore.dao.RegistrationDao;
 import com.capgemini.onlinebookstore.domain.UserBookStoreDto;
 import com.capgemini.onlinebookstore.entities.UserBookStore;
+import com.capgemini.onlinebookstore.exception.EmailIdExistException;
 import com.capgemini.onlinebookstore.exception.EmptyDataException;
 import com.capgemini.onlinebookstore.exception.InvalidDataException;
 import com.capgemini.onlinebookstore.exception.UserNameExistException;
@@ -29,9 +31,6 @@ import com.capgemini.onlinebookstore.mapper.UserBookStoreConverter;
 
 @ExtendWith(SpringExtension.class)
 class RegistrationServiceTest {
-
-
-	
 
 	@Mock
 	private EmailSending mailSender;
@@ -48,15 +47,8 @@ class RegistrationServiceTest {
 	@InjectMocks
 	RegisterServiceImp service=new RegisterServiceImp();
 	
-	@Before
-    public void init() {
-        MockitoAnnotations.initMocks(this);
-    }
- 
-	@Test 
-	public void tempTest() {
-		assertEquals(1,1);
-	}
+	
+
 	@Test
 	public void registerUserTest() throws EmptyDataException, InvalidDataException, UserNameExistException {
 		UserBookStoreDto dto=new UserBookStoreDto();
@@ -72,7 +64,7 @@ class RegistrationServiceTest {
 		Errors errors = new BeanPropertyBindingResult(dto, "dto");
 		UserBookStore user=new UserBookStore();
 		
-		doNothing().when(validator).validate(user, errors);
+		doNothing().when(validator).validate(dto, errors);
 		when(converter.dtoToModel(dto)).thenReturn(user);
 		when(registrationDao.findByUserName(dto.getUserName())).thenReturn(null);
 		when(registrationDao.findByEmailId(dto.getEmailId())).thenReturn(null);
@@ -85,9 +77,7 @@ class RegistrationServiceTest {
 		assertEquals(dto, result);
 	}
 	
-	/*
-	
-	@Test(expected=UserNameExistException.class)
+	@Test
 	public void registerUserWhenUserNameExixstTest() throws EmptyDataException, InvalidDataException, UserNameExistException {
 		UserBookStoreDto dto=new UserBookStoreDto();
 		dto.setFirstName("Harshita");
@@ -102,17 +92,51 @@ class RegistrationServiceTest {
 		Errors errors = new BeanPropertyBindingResult(dto, "dto");
 		
 		UserBookStore user=new UserBookStore();
+		user.setFirstName(dto.getFirstName());
+		user.setLastName(dto.getLastName());
+		user.setUserName(dto.getUserName());
 		
-		doNothing().when(validator).validate(user, errors);
+		doNothing().when(validator).validate(dto, errors);
 		when(converter.dtoToModel(dto)).thenReturn(user);
 		when(registrationDao.findByUserName(dto.getUserName())).thenReturn(Optional.of(user));
-		when(registrationDao.findByEmailId(dto.getEmailId())).thenReturn(null);
+		when(registrationDao.findByEmailId(dto.getEmailId())).thenReturn(Optional.of(user));
 		when(registrationDao.save(user)).thenReturn(user);
 		when(converter.modelToDto(user)).thenReturn(dto);
 		
-		Assertions.assertThrows(UserNameExistException.class, ()->service.registerUser(dto, (BindingResult) errors));
+		assertThrows(UserNameExistException.class, 
+				                () -> service.registerUser(dto, (BindingResult) errors));
+	}
+	
+	@Test
+	public void registerUserWhenEmailIdExixstTest() throws EmptyDataException, InvalidDataException, UserNameExistException {
+		UserBookStoreDto dto=new UserBookStoreDto();
+		dto.setFirstName("Harshita");
+		dto.setLastName("Saxena");
+		dto.setUserName("harshi");
+		dto.setEmailId("harshita5@gmail.com");
+		dto.setPassword("project@5");
+		dto.setConfirmPassword("project@5");
+		dto.setPhoneNumber("9876543210");
+		dto.setUserGender("Female");
+		
+		Errors errors = new BeanPropertyBindingResult(dto, "dto");
+		
+		UserBookStore user=new UserBookStore();
+		user.setFirstName(dto.getFirstName());
+		user.setLastName(dto.getLastName());
+		user.setEmailId(dto.getEmailId());
+		
+		doNothing().when(validator).validate(dto, errors);
+		when(converter.dtoToModel(dto)).thenReturn(user);
+		when(registrationDao.findByUserName(dto.getUserName())).thenReturn(Optional.of(user));
+		when(registrationDao.findByEmailId(dto.getEmailId())).thenReturn(Optional.of(user));
+		when(registrationDao.save(user)).thenReturn(user);
+		when(converter.modelToDto(user)).thenReturn(dto);
+		
+		assertThrows(EmailIdExistException.class, 
+				                () -> service.registerUser(dto, (BindingResult) errors));
 	}
 
-	*/
 
+	
 }
